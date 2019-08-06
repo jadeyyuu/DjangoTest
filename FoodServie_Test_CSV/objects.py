5b9d23be771f51b546326fc8844c5b3df5d5573c
Jade <jadeyyuu@gmail.com>
2019-08-05 21:09:51 -0500

    mfs Extra credit for csv

---
+++ FoodServie_Test_CSV/objects.py
@@ -0,0 +1,14 @@
+class Customer:
+    def __init__(self, Cust_name="", Organization="", Role="", Email="", BldgRoom="",
+                 Address="", Account_No="", City="", State="", Zipcode="",  Phone_Number=""):
+        self.Cust_name = Cust_name
+        self.Organization = Organization
+        self.Role = Role
+        self.Email = Email
+        self.BldgRoom = BldgRoom
+        self.Address = Address
+        self.Account_No = Account_No
+        self.City = City
+        self.State =State
+        self.Zipcode = Zipcode
+        self.Phone_Number= Phone_Number
